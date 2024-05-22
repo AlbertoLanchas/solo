3cfbcf469fc110e87fb8dba97193a61d113fb918
AlbertoLanchas <albertolsn7@gmail.com>
2024-05-22 11:23:08 +0200

    Feat:  Nativewind & Tailwind implemented
    Signed-off-by: AlbertoLanchas <albertolsn7@gmail.com>

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,37 +1,50 @@
-import {
-  DarkTheme,
-  DefaultTheme,
-  ThemeProvider,
-} from "@react-navigation/native";
-import { useFonts } from "expo-font";
+import { DarkTheme, ThemeProvider } from "@react-navigation/native";
+// import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
-import * as SplashScreen from "expo-splash-screen";
+// import * as SplashScreen from "expo-splash-screen";
+// import "react-native-reanimated";
+import { useColorScheme } from "nativewind";
+import "../global.css";
+import { MyLightTheme } from "../utilities/themeOptions";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { useEffect } from "react";
-import "react-native-reanimated";
-
-import { useColorScheme } from "@/hooks/useColorScheme";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
+// SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
-  });
+  // const [loaded] = useFonts({
+  //   SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
+  // });
 
+  // useEffect(() => {
+  //   if (loaded) {
+  //     SplashScreen.hideAsync();
+  //   }
+  // }, [loaded]);
+
+  // if (!loaded) {
+  //   return null;
+  // }
+
+  const { colorScheme, setColorScheme } = useColorScheme();
   useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
+    const loadTheme = async () => {
+      // await AsyncStorage.removeItem('theme');
+      const stored = (await AsyncStorage.getItem("theme")) as ThemeOptions;
+      if (stored) {
+        setColorScheme(stored);
+      } else {
+        // Default to light if nothing or unexpected value is stored
+        setColorScheme("light");
+      }
+    };
 
-  if (!loaded) {
-    return null;
-  }
+    loadTheme();
+  }, [colorScheme]);
 
   return (
-    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : MyLightTheme}>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
